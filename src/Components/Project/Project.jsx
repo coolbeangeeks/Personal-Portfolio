71fa56a770da9a325d3875132215d035c9aa6405
coolbeangeeks <abhigupta96677@gmail.com>
2024-07-05 01:19:59 +0530

    fixed linting erroe

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -4,7 +4,6 @@ import img1 from '../../assets/portfolio 1.png'
 import img2 from '../../assets/potfolio 2.png'
 import img3 from '../../assets/portfolio 3.png'
 import img4 from '../../assets/portfolio 4.png'
-import img5 from '../../assets/portfolio 5.png'
 
 function Project() {
   return (
@@ -19,7 +18,7 @@ function Project() {
           <h3>Movie box: An Movies collection and suggestion with chatgpt</h3>
           <div className="project__item__CTA">
           <a href="https://github.com/coolbeangeeks/movie-box" className='btn'>Github</a>
-          <a href="https://github.com/coolbeangeeks/movie-box" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href="https://github.com/coolbeangeeks/movie-box" className='btn btn-primary' target='_blank'  rel="noopener noreferrer"> Live Demo</a>
 
           </div>
           
@@ -31,7 +30,7 @@ function Project() {
           <h3>Bankist is a fictional and minimalistic online banking application.</h3>
           <div className="project__item__CTA">
           <a href="https://github.com/coolbeangeeks/Bankist-App" className='btn'>Github</a>
-          <a href="https://bankist-bank.vercel.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href="https://bankist-bank.vercel.app/" className='btn btn-primary' target='_blank'  rel="noopener noreferrer">Live Demo</a>
 
           </div>
           
@@ -43,7 +42,7 @@ function Project() {
           <h3>Social media App like and Instragram</h3>
           <div className="project__item__CTA">
           <a href="https://github.com/coolbeangeeks" className='btn'>Github</a>
-          <a href="https://connectify-f27ca.web.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href="https://connectify-f27ca.web.app/" className='btn btn-primary' target='_blank'  rel="noopener noreferrer">Live Demo</a>
 
           </div>
           
@@ -55,7 +54,7 @@ function Project() {
           <h3>An Ecommerce website to browse and purcharse cloth</h3>
           <div className="project__item__CTA">
           <a href="https://github.com/coolbeangeeks/Shopping-Cart" className='btn'>Github</a>
-          <a href="https://coolbeangeeks.github.io/Shopping-Cart/" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href="https://coolbeangeeks.github.io/Shopping-Cart/" className='btn btn-primary' target='_blank'  rel="noopener noreferrer">Live Demo</a>
 
           </div>
           
